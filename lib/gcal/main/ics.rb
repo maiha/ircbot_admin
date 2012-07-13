5fa5775813109691b586dc33660a59b77c1f730e
maiha <maiha@wota.jp>
2012-07-14 07:07:24 +0900

    [refactor] split Gateway::Reminder to gateway and calculate

--- lib/gcal/main/ics.rb
+++ lib/gcal/main/ics.rb
@@ -3,6 +3,7 @@ class Gcal::Main::Ics < Ccp::Invokers::Base
   command Gcal::Gateway::ConfigFile
   command Gcal::Gateway::LoadIcs
   command Gcal::Calculate::Events
+  command Gcal::Calculate::Reminder
   command Gcal::Gateway::DeleteAlerted
   command Gcal::Gateway::DeletePast
   command Gcal::Gateway::Reminder
