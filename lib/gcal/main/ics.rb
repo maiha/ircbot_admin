10f9d1d7eef3cc085a9d635e35728337e2602b54
maiha <maiha@wota.jp>
2012-07-14 06:33:56 +0900

    Added "delete_alerted" option that will delete alerted events if true

--- lib/gcal/main/ics.rb
+++ lib/gcal/main/ics.rb
@@ -3,5 +3,6 @@ class Gcal::Main::Ics < Ccp::Invokers::Base
   command Gcal::Gateway::ConfigFile
   command Gcal::Gateway::LoadIcs
   command Gcal::Calculate::Events
+  command Gcal::Gateway::DeleteAlerted
   command Gcal::Gateway::Reminder
 end
